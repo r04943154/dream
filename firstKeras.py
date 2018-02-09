cc5e717babba8aca4de9b862395ec0c7e9f81fd4
bc <r04943154@ntu.edu.tw>
2018-02-09 23:20:00 +0800

    regress success

--- firstKeras.py
+++ firstKeras.py
@@ -1,17 +1,21 @@
 # Create your first MLP in Keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import optimizers
 import numpy
-import matplotlib.pyplot as plt 
+#import matplotlib.pyplot as plt 
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load pima indians dataset
 dataset = numpy.loadtxt("mul2_add3.csv", delimiter=",")
 # split into input (X) and output (Y) variables
-X = dataset[:,0]
-Y = dataset[:,1]
-plt.scatter(X, Y)
-plt.show()
+#X = dataset[:,0]
+#Y = dataset[:,1]
+X = numpy.linspace(-1, 1, 50)
+numpy.random.shuffle(X)    # randomize the data
+Y = 2 * X + 2 + numpy.random.normal(0, 0.05, (50, ))
+#plt.scatter(X, Y)
+#plt.show()
 Xtrain, Ytrain=X[:40], Y[:40]
 Xtest, Ytest=X[40:], Y[40:]
 # create model
@@ -22,16 +26,17 @@ model.add(Dense(units=1,input_dim=1,use_bias=True, kernel_initializer='glorot_un
 #model.add(Dense(1, activation='sigmoid'))
 
 # Compile model
-model.compile(loss='mse', optimizer='rmsprop')
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='mse', optimizer='sgd')
 for step in range(300):
  cost = model.train_on_batch(Xtrain, Ytrain)
  if step % 10 == 0:
-  print "train cost: ",cost 
-print "\nTesting ------------"
+  print ("train cost: ",cost) 
+print ("\nTesting ------------")
 cost = model.evaluate(Xtest, Ytest, batch_size=10)
-print "test cost:", cost
+print ("test cost:", cost)
 W, b = model.layers[0].get_weights()
-print "Weights=", W, "\nbiases=", b
+print ("Weights=", W, "\nbiases=", b)
 #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
